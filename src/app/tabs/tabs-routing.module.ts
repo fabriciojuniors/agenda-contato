3b35da59b9c2258063b43beb13b8e85834baedc5
Junior Souza <fabriciojuniorsc@gmail.com>
2021-11-28 15:12:06 -0300

    Ajustes iniciais do projeto

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,31 +4,27 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'pagina',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'contatos',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'perfil',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/pagina/contatos',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/pagina/contatos',
     pathMatch: 'full'
   }
 ];
